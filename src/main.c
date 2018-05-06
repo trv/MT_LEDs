259132a447a82674a0fc9f5b550da8abe69f39d9
trv <tim.vanruitenbeek+github@gmail.com>
2018-05-05 20:54:05 -0700

    gradient accel animation

--- src/main.c
+++ src/main.c
@@ -179,9 +179,11 @@ void I2C1_Config(void)
 static volatile uint8_t accelData[6];
 static volatile uint8_t shutdown = 0;
 static volatile uint8_t cycle = 0;
+static volatile uint8_t animate = 0;
 void accel_int1_handler(void *ctx)
 {
     i2c_read(I2C3, ACCEL_ADDR, 0x80 | 0x28, accelData, 6);
+    animate++;
 }
 
 void accel_int2_handler(void *ctx)
@@ -204,7 +206,7 @@ void accel_int2_handler(void *ctx)
 
 static struct LED l;
 
-uint8_t animateIndex = 0;
+uint8_t animateIndex = 8;
 uint8_t animateColor[][3] = {
 		{64,0,0},
 		{48,48,0},
@@ -214,11 +216,43 @@ uint8_t animateColor[][3] = {
 		{48, 0, 48}
 };
 
-void animate(int x, int y, uint8_t *c)
+void solidColors(int x, int y, uint8_t *c)
 {
 	for (int i=0; i < 3; i++) {
 		c[i] = animateColor[animateIndex][i];
 	}
+}
+
+
+uint8_t currentPhase = 0;
+const uint8_t phaseSpeed = 10;
+int panelPhase = 0x80;  // 1/4 of 256
+int redPhase = 0;
+int greenPhase = 85;
+int bluePhase = 170;
+
+uint8_t sinTable[] = {
+  128, 131, 134, 137, 140, 143, 146, 149, 152, 155, 158, 162, 165, 167, 170, 173, 176, 179, 182, 185, 188, 190, 193, 196, 198, 201, 203, 206, 208, 211, 213, 215, 218, 220, 222, 224, 226, 228, 230, 232, 234, 235, 237, 238, 240, 241, 243, 244, 245, 246, 248, 249, 250, 250, 251, 252, 253, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 254, 254, 254, 253, 253, 252, 251, 250, 250, 249, 248, 246, 245, 244, 243, 241, 240, 238, 237, 235, 234, 232, 230, 228, 226, 224, 222, 220, 218, 215, 213, 211, 208, 206, 203, 201, 198, 196, 193, 190, 188, 185, 182, 179, 176, 173, 170, 167, 165, 162, 158, 155, 152, 149, 146, 143, 140, 137, 134, 131, 128, 124, 121, 118, 115, 112, 109, 106, 103, 100, 97, 93, 90, 88, 85, 82, 79, 76, 73, 70, 67, 65, 62, 59, 57, 54, 52, 49, 47, 44, 42, 40, 37, 35, 33, 31, 29, 27, 25, 23, 21, 20, 18, 17, 15, 14, 12, 11, 10, 9, 7, 6, 5, 5, 4, 3, 2, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 3, 4, 5, 5, 6, 7, 9, 10, 11, 12, 14, 15, 17, 18, 20, 21, 23, 25, 27, 29, 31, 33, 35, 37, 40, 42, 44, 47, 49, 52, 54, 57, 59, 62, 65, 67, 70, 73, 76, 79, 82, 85, 88, 90, 93, 97, 100, 103, 106, 109, 112, 115, 118, 121, 124
+};
+
+void animateLEDs(int x, int y, uint8_t *c)
+{
+	// grab the latest accel data TODO: make atomic
+	//int8_t Ax = accelData[1];
+	int8_t Ay = accelData[3];
+	int8_t Az = accelData[5];
+
+	// for now, just use the z-axis to set the gradient
+	//int32_t offset = ((2*x - 7) * Az)/2;
+	int32_t offset = -((2*x - 7) * Az + (2*y - 7) * Ay)/4;
+	if (offset > 127) {
+		offset = 127;
+	} else if (offset < -127) {
+		offset = -127;
+	}
+	c[0] = sinTable[(uint8_t)(currentPhase + offset + redPhase)];
+	c[1] = sinTable[(uint8_t)(currentPhase + offset + greenPhase)];
+	c[2] = sinTable[(uint8_t)(currentPhase + offset + bluePhase)];
 }
 
 int main(void)
@@ -251,7 +285,7 @@ int main(void)
 
     while (1) {
         LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_13);
-        delay_ms(100);
+        delay_ms(10);
         LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_13);
         if (shutdown) {
         	while (1) {
@@ -265,10 +299,15 @@ int main(void)
         }
         if (cycle == 3) {
         	cycle = 0;
-        	LED_Update(&l, animate);
-        	animateIndex = (animateIndex + 1) % 6;
+        	LED_Update(&l, solidColors);
+        	animateIndex = (animateIndex + 1) % 7;
         }
-        delay_ms(100);
+        if (animate >= 2 && animateIndex == 6) {
+        	animate = 0;
+        	LED_Update(&l, animateLEDs);
+        	currentPhase += phaseSpeed;
+        }
+        delay_ms(10);
     }
 
     /* loop forever */
